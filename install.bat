4d7a2f62f89fb0cf3f2d3a416a49a278f13bb5de
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-05-12 19:16:09 +0200

    added error checking

--- install.bat
+++ install.bat
@@ -1,16 +1,21 @@
 @echo off
-set "D=C:\Users\%USERNAME%\Documents\Visual Studio 2012\Templates\ProjectTemplates"
-set "F=%TEMP%\Revit2016AddinWizardCs2.zip"
+if exist cs (goto okcs) else (echo "No cs folder found." && goto exit)
+:okcs
+if exist vb (goto okvb) else (echo "No vb folder found." && goto exit)
+:okvb
+set "D=C:\Users\%USERNAME%\Documents\Visual Studio 2015\Templates\ProjectTemplates"
+set "F=%TEMP%\Revit2017AddinWizardCs0.zip"
 echo Creating C# wizard archive %F%...
 cd cs
 zip -r "%F%" *
 cd ..
 echo Copying C# wizard archive to %D%\Visual C#...
 copy "%F%" "%D%\Visual C#"
-set "F=%TEMP%\Revit2016AddinWizardVb2.zip"
+set "F=%TEMP%\Revit2017AddinWizardVb0.zip"
 echo Creating VB wizard archive %F%...
 cd vb
 zip -r "%F%" *
 cd ..
 echo Copying VB wizard archive to %D%\Visual Basic...
 copy "%F%" "%D%\Visual Basic"
+:exit
