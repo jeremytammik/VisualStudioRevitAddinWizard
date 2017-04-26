889136edd1f046d88a14e70f85b09d65a5cc4cf8
Jeremy Tammik <jeremy.tammik@autodesk.com>
2017-04-26 18:24:11 +0200

    updated VB for Revit 2018

--- install.bat
+++ install.bat
@@ -4,14 +4,14 @@ if exist cs (goto okcs) else (echo "No cs folder found." && goto exit)
 if exist vb (goto okvb) else (echo "No vb folder found." && goto exit)
 :okvb
 set "D=C:\Users\%USERNAME%\Documents\Visual Studio 2015\Templates\ProjectTemplates"
-set "F=%TEMP%\Revit2017AddinWizardCs1.zip"
+set "F=%TEMP%\Revit2018AddinWizardCs0.zip"
 echo Creating C# wizard archive %F%...
 cd cs
 zip -r "%F%" *
 cd ..
 echo Copying C# wizard archive to %D%\Visual C#...
 copy "%F%" "%D%\Visual C#"
-set "F=%TEMP%\Revit2017AddinWizardVb1.zip"
+set "F=%TEMP%\Revit2018AddinWizardVb0.zip"
 echo Creating VB wizard archive %F%...
 cd vb
 zip -r "%F%" *
