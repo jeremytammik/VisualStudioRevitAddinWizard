8d8cf1cae5e0eff2c911be9a076da6c3c4d12022
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-09-06 12:58:49 +0200

    migrated vb template to Revit 2019

--- vb/My Project/AssemblyInfo.vb
+++ vb/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2018.0.0.0")>
-<Assembly: AssemblyFileVersion("2018.0.0.0")>
+<Assembly: AssemblyVersion("2019.0.0.0")>
+<Assembly: AssemblyFileVersion("2019.0.0.0")>
