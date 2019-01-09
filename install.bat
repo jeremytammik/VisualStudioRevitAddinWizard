8efa7397daedc5360c5c11548ccdec198babd0bb
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-01-09 17:12:51 +0100

    incremented oopyright year to 201

--- install.bat
+++ install.bat
@@ -4,14 +4,14 @@ if exist cs (goto okcs) else (echo "No cs folder found." && goto exit)
 if exist vb (goto okvb) else (echo "No vb folder found." && goto exit)
 :okvb
 set "D=%userprofile%\Documents\Visual Studio 2015\Templates\ProjectTemplates"
-set "F=%TEMP%\Revit2019AddinWizardCs0.zip"
+set "F=%TEMP%\Revit2019AddinWizardCs1.zip"
 echo Creating C# wizard archive %F%...
 cd cs
 zip -r "%F%" *
 cd ..
 echo Copying C# wizard archive to %D%\Visual C#...
 copy "%F%" "%D%\Visual C#"
-set "F=%TEMP%\Revit2019AddinWizardVb0.zip"
+set "F=%TEMP%\Revit2019AddinWizardVb1.zip"
 echo Creating VB wizard archive %F%...
 cd vb
 zip -r "%F%" *
